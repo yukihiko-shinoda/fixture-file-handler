d2ae3af71d0988624974c9e0974817d67facfa02
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-08-12 20:10:01 +0900

    Order keys by alphabet

--- setup.py
+++ setup.py
@@ -9,24 +9,8 @@ def main():
         readme = file.read()
 
     setup(
-        name="fixturefilehandler",
-        version="1.2.0",
-        description=(
-            "This project helps you to vacate specific file path "
-            "or deploy resource file into specific file path when unit testing."
-        ),
-        long_description=readme,
-        long_description_content_type="text/markdown",
         author="Yukihiko Shinoda",
         author_email="yuk.hik.future@gmail.com",
-        packages=find_packages(exclude=("tests*", "yourproduct*")),
-        package_data={"fixturefilehandler": ["py.typed"]},
-        python_requires=">3.6.0",
-        install_requires=[],
-        dependency_links=[],
-        setup_requires=["pytest-runner"],
-        url="https://github.com/yukihiko-shinoda/fixture-file-handler",
-        keywords="fixture file vacate deploy unittest pytest testing",
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
@@ -42,6 +26,22 @@ def main():
             "Topic :: System :: Filesystems",
             "Typing :: Typed",
         ],
+        dependency_links=[],
+        description=(
+            "This project helps you to vacate specific file path "
+            "or deploy resource file into specific file path when unit testing."
+        ),
+        install_requires=[],
+        keywords="fixture file vacate deploy unittest pytest testing",
+        name="fixturefilehandler",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        packages=find_packages(exclude=("tests*", "yourproduct*")),
+        package_data={"fixturefilehandler": ["py.typed"]},
+        python_requires=">3.6.0",
+        setup_requires=["pytest-runner"],
+        url="https://github.com/yukihiko-shinoda/fixture-file-handler",
+        version="1.2.0",
     )
 
 
